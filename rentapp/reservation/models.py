8575e7669c41ce9c174c7ebfc449db2958f82815
JosueGauthier <josue.gauthier@gmail.com>
2022-11-25 18:14:22 +0100

    add models

--- rentapp/reservation/models.py
+++ rentapp/reservation/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.timezone import now
+from django.contrib.auth.models import User
+
+from rentapp.organization.models import Product
+
+
+class Reservation(models.Model):
+    """
+    Define a Reservation.
+
+    """
+    start_date = models.DateTimeField('Date', default=now)
+    end_date = models.DateTimeField('Date', default=now)
+    renter = models.ForeignKey(User,
+                               on_delete=models.CASCADE)
+    renter = models.ForeignKey(Product,
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        """
+        Return the display name of the Sale.
+
+        :returns: pk of sale
+        :rtype: string
+        """
+        return 'Achat ' + self.shop.__str__() + ', ' + self.string_products()
